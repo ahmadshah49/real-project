8f3df5614d146e9053f9abeb922cb20ddf60be6e
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-01-10 11:19:27 +0500

    any

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Navbar from "../Navbar/Navbar";
-import { FaPhoneAlt } from "react-icons/fa";
 
 const Hero = () => {
   return (
     <div className="bgImg  relative h-screen">
       <div className="absolute w-full h-screen bg-black/90">
-        {/* <Navbar /> */}
+        <Navbar />
         <div className="w-full  h-full text-white flex  flex-col items-center justify-center">
           <Image
             src={"/logo.PNG"}
@@ -25,19 +24,10 @@ const Hero = () => {
           <div className="my-6  font-bold rounded-2xl wow animate-bounce bg-red-600/40  py-4 px-2">
             <button>We're Coming soon</button>
           </div>
-          <div className="w-full text-center bg-black/40 px-4 absolute text-xs flex sm:gap-1 gap-4 items-center justify-center bottom-0 text-white">
+          <div className="w-full text-center bg-black/40 px-4  absolute text-xs flex  items-center justify-center gap-4  bottom-0 text-white">
             <h2 className="text-xs sm:text-[12px] my-4">
-              Galaxy ISP(Pvt) Limited. <sub className="text-[12px]">©</sub>
+              Galaxy | ISP & CATV | Copyright 2024 | All Rights Reserved.
             </h2>
-            |
-            <div className="flex text-sm sm:text-[12px] gap-1 items-center">
-              <FaPhoneAlt />
-              <span className="text-base max-sm:text-[12px]">
-                <p className="text-xs max-sm:text-[12px]">0300 8652993</p>
-                {/* <p className="text-xs sm:text-[10px]">0323 1110777</p> */}
-              </span>
-            </div>
-            |<h1 className="text-xs"> Copyright © 2024</h1>
           </div>
         </div>
       </div>
