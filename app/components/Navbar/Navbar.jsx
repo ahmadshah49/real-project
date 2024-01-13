9a2c8902372300b04fe8be12da35374331bee50c
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-01-13 17:52:17 +0500

    main

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -1,19 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { FaPhoneAlt } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="text-white  fixed shadow-md shadow-white/20 wow w-full bg-white/30 flex items-center justify-between py-2 px-6 text-2xl">
-      <Image src={"/logo.PNG"} width={500} height={500} className="w-16 h-12" />
-      <div className="flex text-sm sm:text-[12px] gap-1 items-center">
-        <FaPhoneAlt />
-        <span className="text-base max-sm:text-[12px]">
-          <p className="text-xs max-sm:text-[12px]">0300 8652993</p>
-          {/* <p className="text-xs sm:text-[10px]">0323 1110777</p> */}
-        </span>
+    <>
+      <div className="px-4 h-24 bg-black">
+        <div>
+          <div>
+            <Image src={"/logo.PNG"} width={80} height={80} />
+          </div>
+          <div>
+            <Link>Home</Link>
+            <Link>Prices</Link>
+            <Link>Services</Link>
+            <Link>Support</Link>
+          </div>
+        </div>
+        <div></div>
       </div>
-    </div>
+    </>
   );
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      {/* <Hero /> */}
+      <Navbar />
     </>
   );
 }
