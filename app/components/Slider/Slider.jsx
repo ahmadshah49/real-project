f2bc80821e0833e99ef48b9c49b1f5b3dfc50e76
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-01-15 18:25:19 +0500

    ok

--- app/components/Slider/Slider.jsx
+++ app/components/Slider/Slider.jsx
@@ -5,7 +5,7 @@ const Slider = ({ title, pera, src, width, height }) => {
   return (
     <div className=" px-4 w-full h-full flex max-sm:flex-col  items-center justify-center">
       <div className="w-[50%] px-6 flex flex-col justify-center max-sm:w-full">
-        <h1 className="max-sm:text-6xl  max-md:text-5xl  lg:text-7xl py-4 text-[#E77A18] font-extrabold">
+        <h1 className="max-sm:text-5xl  max-md:text-5xl  lg:text-7xl py-4 text-[#E77A18] font-extrabold">
           {title}
         </h1>
         <p className="italic p w-[60%] max-sm:w-full">{pera}</p>
